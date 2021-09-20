f1461e2114f717db5977b5690e05988200c7ce75
saha80 <48954743+saha80@users.noreply.github.com>
2021-09-20 23:17:02 +0300

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rock_paper_scissors_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
